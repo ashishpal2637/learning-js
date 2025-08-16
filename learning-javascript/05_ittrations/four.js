01edbe90700bbc04f5a84da5de47abfe2bed8ba1
ashishpal2637 <ashishpal2637@gmail.com>
2025-08-16 18:05:49 +0530

    for in loop in js

---
+++ learning-javascript/05_ittrations/four.js
@@ -0,0 +1,30 @@
+const myobj = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    py: 'python'
+}
+
+for (const key in myobj) {
+    // console.log(`${key} is shortcut for ${myobj[key]}`);  
+}
+
+const programming = ["js", "java", "python", "cpp"]
+
+for (const key in programming) {
+//   console.log(programming[key]);  
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United states of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//    console.log(key);
+// }
+ 
+
+// for in loop object and array  par lgate hai
+// for in map par nahi kaam nahi karta 
