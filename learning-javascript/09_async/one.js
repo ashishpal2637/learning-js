42ab62f12d79ed034d441da49a2a00950e9b7036
ashishpal2637 <ashishpal2637@gmail.com>
2025-08-29 14:05:00 +0530

    Async in js notes

---
+++ learning-javascript/09_async/one.js
@@ -0,0 +1,26 @@
+// --> Javascript :->
+//     -> synchronous          |
+//     -> single threaded      | ->  Default
+
+// --> execution context 
+//      |
+//      | -> execute one line of code at a time 
+
+//      |
+//      |--> console log --> 1          Each operation waits for the last one to compile before executing
+//      |
+//      |--> console log --> 2
+//      |
+//      |
+//      |    CALL Stack               Memory Heap 
+//      |
+
+
+//    Blocking Code                    vs            Non Blocking Code
+//         |                                              |
+//    Block the flow of Program                      Does not block execution
+//         |                                             |
+//    Read file Sync                                 Read file Async    
+
+      
+
